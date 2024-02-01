dc13b400579e2732ab1e4eed4e4a2b4ae65ad8fa
VictorTroyAvila <116412427+VictorTroyAvila@users.noreply.github.com>
2024-02-02 01:27:46 +0800

    Madaming ginawa onti lang nang yari

--- javascript/Article.js
+++ javascript/Article.js
@@ -66,6 +66,15 @@ function LH() {
     }
 }
 
+function APPLCS() {
+    var y = document.getElementById("choices");
+    if (y.style.display === "none") {
+    y.style.display = "block";
+    } else {
+    y.style.display = "none";
+    }
+}
+
 function incrementValue()
 {
     var i = 0;
@@ -80,4 +89,80 @@ function decrementValue()
     function buttonClick() {
         document.getElementById('inc').value = --i;
     }
+}
+
+function calcTotal() {
+    var total = document.getElementById('ttl');
+
+    var watts = +document.getElementById('wtz').value;
+    
+    var utilityRate = +document.getElementById('kWh').value;
+    
+    var hrs = +document.getElementById('hPd').value;
+
+    var days = +document.getElementById('dPy').value;
+
+    var product = watts * utilityRate * hrs * days;
+
+    total.value = product + " php/yr";
+}
+
+function applianceChoices(clicked) 
+{
+    document.getElementById(clicked).addEventListener('click', function() {
+        var text = document.getElementById('apl');
+        var txtWatts = document.getElementById('wtz');
+
+        if (text.value == '')
+        {
+            text.value += clicked;
+        }
+        else
+        {
+            text.value = '';
+            text.value += clicked;
+        }
+
+        if (text.value == 'Refrigerator')
+        {
+            txtWatts.value = 430 
+        }
+        else if (text.value == 'Air Conditioner')
+        {
+            txtWatts.value = 3000 
+        }
+        else if (text.value == 'Television')
+        {
+            txtWatts.value = 150 
+        }
+        else if (text.value == 'Blender')
+        {
+            txtWatts.value = 400 
+        }
+        else if (text.value == 'Microwave')
+        {
+            txtWatts.value = 1200 
+        }
+        else if (text.value == 'Oven')
+        {
+            txtWatts.value = 2400 
+        }
+
+        calcTotal();
+    });
+}
+
+function enter() {
+    var input = document.getElementById("kWh");
+    
+    input.addEventListener("keypress", function(event) {
+        if (event.key == "Enter") {
+        event.preventDefault();
+        input.value = '';
+        }
+        else
+        {
+            input.value = 'did not work';
+        }
+});
 }
