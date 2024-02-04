210ca1c2e09eec4e617aac4b28bee7b213f6c41e
Nico Ampoloquio <116412203+NicoAmpoloquio@users.noreply.github.com>
2024-02-04 19:31:07 +0800

    added dba

--- crazy-nicooo/EnerTrackHub/html/login.php
+++ crazy-nicooo/EnerTrackHub/html/login.php
@@ -1,28 +1,56 @@
+<?php
+include('dbconfig.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['user_login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $conn->prepare("SELECT email, password FROM user WHERE email = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows == 1) {
+        // replace with your actual code
+        header('Location: user_profile.html');
+
+        exit();
+    } else {
+        $error[] = 'Incorrect username or password';
+    }
+}
+
+$conn->close();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WattWise</title>
     <link rel="stylesheet" href="../html/css/login.css">
 </head>
+
 <body>
     <header>
         <div>
             <img src="../html/images/logo.png" alt="Logo">
             <h1>WattWise</h1>
         </div>
-        <a href="#">Go</a>
+        <a href="#">Log in</a>
     </header>
     <div class="container">
         <div class="left-container">
             Log In for a Sustainable Future
         </div>
         <div class="login-container">
-            <form class="login-form" action="../html/database/login_process.php" method="POST">
+            <form class="login-form" action="" method="POST">
                 <h4>Log In</h4>
-                <label for="username" class="username-label">Username</label>
-                <input type="text" id="username" name="username" placeholder="Enter your username">
+                <label for="username" class="username-label">Email</label>
+                <input type="email" id="username" name="username" required>
 
                 <label for="password" class="password-label">Password
                     <div class="password-toggle" onclick="togglePasswordVisibility()">
@@ -31,14 +59,15 @@
                 </label>
 
                 <div class="password-input-container">
-                    <input type="password" id="password" name="password" placeholder="Enter your password">
+                    <input type="password" id="password" name="password" required>
                 </div>
+                <!-- add some style -->
+                
 
-                <button type="submit" id="login-btn" class="btn">Log in</button>
-                <p class="error-message" id="error-message" style="text-align: center;"></p>
+                <button type="submit" name="user_login">Log in</button>
 
                 <div class="additional-info">
-                    By continuing, you agree to the <a href="#" class="condition">Terms of use</a> and <a href="#" class="condition">Privacy Policy</a>. 
+                    By continuing, you agree to the <a href="#" class="condition">Terms of use</a> and <a href="#" class="condition">Privacy Policy</a>.
                     <br>
                     <br>
                     <br>
@@ -70,12 +99,13 @@
         }
 
         function redirectToSignup() {
-            window.location.href = "../html/signup.html";
+            window.location.href = "signup.php";
         }
 
         function redirectToUser() {
-            window.location.href = "../html/user_profile.html";
+            window.location.href = "user_profile.html";
         }
     </script>
 </body>
+
 </html>
